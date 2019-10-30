156bcdab8f0f93cde683247c931e714378985798
Shad Mirza <iamshadmirza@gmail.com>
2019-10-30 14:48:30 +0530

    Input style updated

--- Input/Input.js
+++ Input/Input.js
@@ -7,32 +7,30 @@ const Input = (props) => {
   const labelSize = props.size ? { fontSize: props.size * 0.8 } : {};
   const inputSize = props.size ?
     { fontSize: props.size, paddingVertical: props.size < 11 ? 0 : 5 } :
-    {};
+  {};
   const floatingStyle = props.floatingLabel && props.value.length === 0 ?
     { display: 'none' } :
   {};
   const colorStyle = props.color ? { borderBottomColor: props.color } : {};
   const roundStyle = props.round ? styles.roundStyle : {};
+
   return (
     <View style={props.containerStyle}>
-      {props.label ?
+      {props.label &&
         <Text style={StyleSheet.flatten([styles.label, props.labelStyle, floatingStyle, labelSize])}>
           {props.label}
-        </Text> :
-      null}
-      <View style={[styles.inputContainer, props.style]}>
-        {props.leftIcon ?
-          <View style={styles.icon}>{props.leftIcon}</View> :
-        null}
+        </Text>}
+      <View style={[styles.inputContainer, roundStyle, props.style]}>
+        {props.leftIcon &&
+          <View style={styles.leftIcon}>{props.leftIcon}</View>}
         <TextInput
           editable={!props.disabled}
           {...props}
-          style={StyleSheet.flatten([styles.input, colorStyle, roundStyle, inputSize])}
+          style={StyleSheet.flatten([styles.input, colorStyle, inputSize])}
           placeholder={props.floatingLabel ? props.label : props.placeholder}
         />
-        {props.rightIcon ?
-          <View style={styles.icon}>{props.rightIcon}</View> :
-        null}
+        {props.rightIcon &&
+          <View style={styles.rightIcon}>{props.rightIcon}</View>}
       </View>
     </View>
   );
@@ -42,15 +40,15 @@ const styles = StyleSheet.create({
   inputContainer: {
     flexDirection: 'row',
     alignItems: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: colors.grey[400],
   },
   input: {
     flex: 1,
     padding: 5,
     borderRadius: 3,
-    paddingVertical: 8,
-    paddingHorizontal: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: colors.grey[400],
+    paddingVertical: 7.5,
+    paddingHorizontal: 15,
     fontSize: DEFAULT_SIZE,
     color: '#333333',
   },
@@ -59,8 +57,13 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     color: '#666666',
   },
-  icon: {
-    padding: 5,
+  leftIcon: {
+    paddingLeft: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  rightIcon: {
+    paddingRight: 10,
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -68,7 +71,6 @@ const styles = StyleSheet.create({
     backgroundColor: colors.grey[300],
     borderBottomWidth: 0,
     borderRadius: 50,
-    paddingHorizontal: 15,
   },
 });
 
