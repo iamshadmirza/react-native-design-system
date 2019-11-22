d8eeaf05f03488eeac5bf11d8209890c3b56d44c
Shad Mirza <iamshadmirza@gmail.com>
2019-11-22 19:29:15 +0530

    Header modified

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import { View, TouchableOpacity, Text, StyleSheet, Platform } from 'react-native';
+import { View, TouchableOpacity, TouchableNativeFeedback, Text, StyleSheet, Platform, StatusBar } from 'react-native';
 import PropTypes from 'prop-types';
+import withTheme from '../util/withTheme';
+
+
+const getContainerStyle = ({ theme, color }) => {
+  const headerStyle = [styles.container];
+  headerStyle.push({
+    backgroundColor: theme.brandColor[color],
+  });
+  return headerStyle;
+};
+
+const getTextStyle = ({ theme, color, textAlign, fontSize }) => {
+  const textStyle = [styles.text];
+  textStyle.push({
+    backgroundColor: theme.brandColor[color],
+    fontSize: theme.fontSize[fontSize],
+  });
+  if (textAlign) {
+    textStyle.push({
+      textAlign: textAlign,
+    });
+  }
+  return textStyle;
+};
 
 const Header = (props) => {
-  const textPlacement = props.textAlign ? { textAlign: props.textAlign } : {};
-  const colorStyle = props.color ? { backgroundColor: props.color } : {};
-  const heightStyle = props.height ? { height: props.height } : {};
+  const TouchableElement = Platform.OS === 'android' ? TouchableNativeFeedback : TouchableOpacity;
   return (
-    <View style={StyleSheet.flatten([styles.container, heightStyle, colorStyle, props.style])}>
+    <View style={StyleSheet.flatten([getContainerStyle(props), props.style])}>
+      <StatusBar
+        barStyle={props.barStyle}
+        backgroundColor={props.theme.brandColor[props.barColor]}
+      />
       {props.leftIcon &&
-        <TouchableOpacity onPress={props.onLeftIconPress} style={[styles.iconStyle, props.iconStyle]}>
-          {props.leftIcon}
-        </TouchableOpacity>}
-      <Text style={StyleSheet.flatten([styles.text, textPlacement, props.textStyle])}>
+        <TouchableElement onPress={props.onLeftIconPress}>
+          <View style={StyleSheet.flatten([styles.iconStyle, props.iconStyle])}>
+            {props.leftIcon}
+          </View>
+        </TouchableElement>
+      }
+      <Text style={StyleSheet.flatten([getTextStyle(props), props.textStyle])}>
         {props.children}
       </Text>
       {props.rightIcon &&
-        <TouchableOpacity onPress={props.onRightIconPress} style={[styles.iconStyle, props.iconStyle]}>
-          {props.rightIcon}
-        </TouchableOpacity>}
+        <TouchableElement onPress={props.onRightIconPress}>
+          <View style={StyleSheet.flatten([styles.iconStyle, props.iconStyle])}>
+            {props.rightIcon}
+          </View>
+        </TouchableElement>
+      }
     </View>
   );
 };
@@ -27,40 +59,53 @@ Header.propTypes = {
   style: PropTypes.object,
   textStyle: PropTypes.object,
   textAlign: PropTypes.oneOf(['auto', 'left', 'center', 'right', 'justify']),
+  fontSize: PropTypes.oneOf(['xxsmall', 'xsmall', 'small', 'medium', 'large', 'xlarge', 'xxlarge']),
   children: PropTypes.string,
   color: PropTypes.string,
-  height: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
   leftIcon: PropTypes.element,
   onleftIconPress: PropTypes.func,
   rightIcon: PropTypes.element,
   onRightIconPress: PropTypes.func,
   iconStyle: PropTypes.object,
+  barColor: PropTypes.string,
+  barStyle: PropTypes.oneOf(['default', 'dark-content', 'light-content']),
 };
 
 Header.defaultProps = {
   children: 'Home',
-  textAlign: Platform.OS === 'android' ? 'left' : 'center',
+  color: 'primary',
+  barColor: 'primary',
+  barStyle: 'default',
+  fontSize: 'medium',
 };
 
 const styles = StyleSheet.create({
   container: {
     elevation: 3,
-    backgroundColor: '#333',
     width: '100%',
-    height: Platform.OS === 'android' ? 56 : 64,
+    height: Platform.select({
+      android: 56,
+      ios: 64,
+    }),
     flexDirection: 'row',
     alignItems: 'center',
+    paddingHorizontal: 10,
   },
   text: {
     flex: 1,
-    fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: Platform.select({
+      android: 'bold',
+      ios: '500',
+    }),
+    textAlign: Platform.select({
+      android: 'left',
+      ios: 'center',
+    }),
     color: '#fff',
-    paddingHorizontal: 10,
   },
   iconStyle: {
     padding: 5,
   },
 });
 
-export default Header;
+export default withTheme(Header);
