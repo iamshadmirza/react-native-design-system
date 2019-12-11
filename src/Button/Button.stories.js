d46253f21d4458aeee0a6b769c5dce84073fcc2e
Shad Mirza <iamshadmirza@gmail.com>
2019-12-11 07:02:26 +0530

    story changes

--- src/Button/Button.stories.js
+++ src/Button/Button.stories.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react-native';
+import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { Button } from './index';
 
 storiesOf('Button', module)
   .addParameters({
     info: {
-      source: true
-    }
+      source: true,
+    },
   })
   .add('default', () => (
     <Button onPress={action('pressed')}>
