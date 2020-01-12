95d5f9ddd9347b3ba4199a4921f3e74e02aeed43
Shad <iamshadmirza@gmail.com>
2020-01-12 18:09:58 +0530

    docs generated

---
+++ docs/precache-manifest.efa9a092ceb299b970eeeb210b78483f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4335e5d72ce30f61507f7192df6e744a",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.65edb0b22f289d6202c7.bundle.js"
+  },
+  {
+    "url": "runtime~main.65edb0b22f289d6202c7.bundle.js"
+  },
+  {
+    "url": "vendors~main.65edb0b22f289d6202c7.bundle.js"
+  },
+  {
+    "url": "vendors~main.65edb0b22f289d6202c7.bundle.js.LICENSE"
+  }
+]);
