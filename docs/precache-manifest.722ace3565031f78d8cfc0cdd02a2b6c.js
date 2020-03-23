220267524774f472fa4fd0e3f5dcd0ae4b68a9d1
Vytenis Kuciauskas <projektas@gmail.com>
2020-03-23 21:11:53 +0200

    Update docs

---
+++ docs/precache-manifest.722ace3565031f78d8cfc0cdd02a2b6c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35822b3caabbf1a278dc56c59e7c6a23",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.a9097def21e31abbd0ae.bundle.js"
+  },
+  {
+    "url": "runtime~main.a9097def21e31abbd0ae.bundle.js"
+  },
+  {
+    "url": "vendors~main.a9097def21e31abbd0ae.bundle.js"
+  },
+  {
+    "url": "vendors~main.a9097def21e31abbd0ae.bundle.js.LICENSE.txt"
+  }
+]);
